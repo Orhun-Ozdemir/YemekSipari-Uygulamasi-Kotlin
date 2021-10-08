13de11509301b2132057a165b4e5d730aa106db8
Orhun Ozdemir <orhun.ozdemirrr@gmail.com>
2021-10-09 01:21:46 +0300

    Proje Bitis

--- app/src/main/java/com/example/yemeksiparis/fragment/SplashFragment.kt
+++ app/src/main/java/com/example/yemeksiparis/fragment/SplashFragment.kt
@@ -5,19 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.yemeksiparis.R
+import com.example.yemeksiparis.databinding.FragmentSplashBinding
 
 
 class SplashFragment : Fragment() {
-
+   private lateinit var tasarim:FragmentSplashBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var tasarim=inflater.inflate(R.layout.fragment_splash, container, false)
+        tasarim= FragmentSplashBinding.inflate(inflater)
+        tasarim.splashFragment=this
 
 
-        return tasarim
+
+
+        return tasarim.root
+    }
+
+
+    fun AnaSayfa()
+    {
+
+    val direction=SplashFragmentDirections.actionSplashFragmentToAnaSayfaFragment()
+        Navigation.findNavController(tasarim.root).navigate(direction)
+
     }
 
     
